e59b7ff939cae0ed1c9279a79c540248e748b175
markdrehmann <markdrehmann@gmail.com>
2021-03-22 21:41:40 -0500

    commit tree 577b2db3505db330ecd9a64f147cb5941ae35b4c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function mapToNegativize(sourceArray) { return sourceArray.map(element => element * -1) }
+function mapToNoChange(sourceArray) { return sourceArray}
+function mapToDouble(sourceArray) { return sourceArray.map(element => element * 2) }
+function mapToSquare(sourceArray) { return sourceArray.map(element => element ** 2) }
+
+function func(total, num) { return total + num }
+function reduceToTotal(sourceArray, startingPoint=0) {return sourceArray.reduce(func, startingPoint)}
+
+function allTruthyFunc(current, el) { return !!current && !!el }
+function anyTruthyFunc(current, el) { return !!current || !!el }
+function reduceToAllTrue(src) { return src.reduce(allTruthyFunc) }
+function reduceToAnyTrue(src) { return src.reduce(anyTruthyFunc) }
